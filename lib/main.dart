e115966ab898deebac17159eb7070b39c3cba8c6
komal <kk6612465@gmail.com>
2022-10-05 19:48:46 +0530

    diceroll

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dice Roller App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: HomePage(),
+    );
+  }
+}
